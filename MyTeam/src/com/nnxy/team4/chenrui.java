1d310793c2b6815b9149b326aea330ee8b0f1767
love-sang <1522580013@qq.com>
2020-12-09 18:27:52 +0800

    tijiao

--- MyTeam/src/com/nnxy/team4/chenrui.java
+++ MyTeam/src/com/nnxy/team4/chenrui.java
@@ -1,5 +1,16 @@
 package com.nnxy.team4;
 
 public class chenrui {
+	
+	private int id;
+	
+	public int fetId() {
+		return id;
+	}
+	
+	public chenrui(int id) {
+		super();
+		this.id = id;
+	}
 
 }
